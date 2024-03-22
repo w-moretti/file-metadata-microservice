25db2dde0879731e9bd351e80c4b5235bca92290
William Moretti <maleck3@hotmail.com>
2024-03-22 14:06:52 +0000

    Changes to pass all tests

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 var app = express();
 
 app.use(cors());
@@ -11,8 +14,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
